67cef710fb29a29e1423265da3fa783e3e312efa
mohammadhosseintorkan <86060493+mohammadhosseintorkan@users.noreply.github.com>
2021-06-25 14:03:09 +0430

    پروژه نسخه c5.1.0

---
+++ poshak/includes/Product.php
@@ -0,0 +1,32 @@
+<?php
+	if(! class_exists('Product')) {
+		class Product
+		{
+			static function columnsList( $vars, $sep = ', ' ){
+				$columns = array_keys( $vars );
+				return join($sep , $columns);
+			}
+			static function valuesList( $vars, $sep = "', '" ){
+				$values = array_values( $vars );
+				return "'" . join($sep , $values) . "'";
+			}
+			static function add( $params ){
+				$columnsString = self::columnsList( $params );
+				$valuesString = self::valuesList( $params );
+				$tableName = __CLASS__;
+				
+				$sql = "INSERT INTO {$tableName}({$columnsString}) 
+						VALUES({$valuesString})";
+				$result = $GLOBALS['db'] -> execute( $sql );
+
+
+				if( $result )
+					alerts('محصول با موفقیت ثبت شد!', 'success');
+
+			}
+			static function update(){}
+			static function delete(){}
+			static function find(){}
+		}
+	}
+?>
---
+++ poshak/public/index.php
@@ -0,0 +1,5 @@
+<p>home</p>
+
+<a href="addProduct.html">افزودن محصول</a><br>
+<a href="contact.html">تماس با ما</a><br>
+<a href="register.html">ثبت نام</a><br>
